3a5910013ed04beeb407aeca0dbadeb3e87b1cd6
David Newman <dnewmanau@gmail.com>
2023-08-24 16:37:13 +1000

    Basic program

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # csv-duplicate-remover
+
+## Requirements
+
+`pip install pandas`
+
+## Run
+
+`python csv-duplicate-remover.py`
---
+++ csv-duplicate-remover.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def main():
+    # Open the CSV file
+    print("CSV relative path: ")
+    inp = input()
+    file = open(inp, "r")
+
+    # Convert CSV file to pandas dataframe
+    df = pd.read_csv(file)
+
+    # Remove duplicate rows
+    df.drop_duplicates(subset=None, inplace=True)
+
+    # Output duplicates removed to new CSV
+    df.to_csv("duplicates_removed.csv", index=False)
+
+if __name__ == "__main__":
+    main()
